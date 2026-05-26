565312a59675137d39ceb6535e3c46d627db0b6f
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2026-05-26 18:05:02 +0900

    Initial commit

---
+++ scripts/simple_move.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+# -*- coding: utf_8 -*-
+
+import math
+
+import rclpy
+from geometry_msgs.msg import PoseWithCovarianceStamped, Twist
+from nav_msgs.msg import Odometry
+from oit_robot_utils.pose_conversions import pose2d_from_amcl
+from rclpy.node import Node
+from sensor_msgs.msg import LaserScan
+
+# https://docs.ros.org/en/jazzy/Tutorials/Beginner-Client-Libraries/Writing-A-Simple-Py-Publisher-And-Subscriber.html#write-the-publisher-node
+
+
+class SimpleMove(Node):
+    def __init__(self):
+        super().__init__('simple_move')
+
+        # パブリッシャー。ロボットに速度の指令を送る道具。
+        self.pub_cmd_vel = self.create_publisher(Twist, 'cmd_vel', 10)
+        # サブスクライバー。センサの情報の受信器。データが届くと、callback関数が呼ばれる。
+        self.sub_scan = self.create_subscription(
+            LaserScan, 'scan', self.scan_callback, 10)
+        self.sub_amcl_pose = self.create_subscription(
+            PoseWithCovarianceStamped, 'amcl_pose', self.amcl_pose_callback, 10)
+        self.sub_odom = self.create_subscription(
+            Odometry, 'odom', self.odom_callback, 10)
+        # コールバックで受信したデータの入れ物
+        self.scan = None  # レーザースキャン
+        self.pose = None  # 地図中の位置姿勢
+        self.odom = None  # オドメトリの位置姿勢と速度
+
+        # タイマー（20Hz: 0.05秒間隔）。一定周期で指定された関数を呼ぶ。
+        self.timer = self.create_timer(0.05, self.waiting_loop)  # 最初は待ちループ
+
+        # 状態と変数
+        self.start_time = None  # プログラムの開始時刻。
+
+    def scan_callback(self, msg):
+        # コールバックではメッセージ保存のような軽い処理しかしない。
+        self.scan = msg
+
+    def amcl_pose_callback(self, msg):
+        self.pose = pose2d_from_amcl(msg)
+
+    def odom_callback(self, msg):
+        self.odom = msg
+
+    def waiting_loop(self):
+        # サブスクライバーが接続されるのを待つループ。接続されるまでは何もしない。
+        if self.pub_cmd_vel.get_subscription_count() == 0:
+            self.get_logger().info("Waiting for subscriber...", throttle_duration_sec=1.0)
+            return
+        self.destroy_timer(self.timer)
+        self.get_logger().info("Subscriber connected. Starting control loop.")
+        self.start_time = self.get_clock().now()
+        self.timer = self.create_timer(
+            0.05, self.control_loop)  # コントロールループに切り替える
+
+    def control_loop(self):
+        # タイマーで呼ばれる関数。ここにロボットの制御のコードを書く。
+        tm = self.get_clock().now()
+        from_start = (tm - self.start_time).nanoseconds / 1e9
+        if self.pose:
+            # 地図中の現在位置の表示
+            self.get_logger().info(
+                f'{from_start:.2f}sec, (x, y, theta): {self.pose.x:.2f}, {self.pose.y:.2f}, {math.degrees(self.pose.theta):.2f}')
+
+        msg = Twist()
+        # 動作の継続時間や速度を色々変更してみましょう。
+        # 速度に負の数を入れてみましょう。
+        # elif節を増やして様々な動きをさせましょう。例：正方形を描かせる。
+        if from_start < 5.0:  # 最初の5秒は前進
+            msg.linear.x = 0.2  # 並進速度(m/s)
+        elif from_start < 8.0:  # 次の3秒は旋回
+            msg.angular.z = math.radians(30)  # 旋回角速度(ラジアン/s)
+        # elif from_start < 11.0:  # 次の3秒の動作。並進速度と旋回角速度を両方与えることもできます。
+        #    msg.linear.x = 0.2  # 並進速度(m/s)
+        #    msg.angular.z = math.radians(30)  # 旋回角速度(ラジアン/s)
+        else:
+            self.get_logger().info("Target time reached. Stopping.")
+            self.pub_cmd_vel.publish(msg)  # 停止指令を送る
+            raise KeyboardInterrupt()     # 終了
+        self.pub_cmd_vel.publish(msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = None
+    try:
+        node = SimpleMove()
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        if node:
+            node.destroy_node()
+        if rclpy.ok():
+            rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/simple_navigation.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# -*- coding: utf_8 -*-
+
+import rclpy
+from geometry_msgs.msg import PoseWithCovarianceStamped, Twist
+from nav2_msgs.action import NavigateThroughPoses
+from oit_robot_utils.pose_conversions import (pose2d_from_amcl,
+                                              pose_stamped_from)
+from rclpy.action import ActionClient
+from rclpy.node import Node
+
+
+class NavCommander(Node):
+    def __init__(self):
+        super().__init__('nav_commander')
+        # パブリッシャー。ロボットに速度の指令を送る道具。
+        self.pub_cmd_vel = self.create_publisher(Twist, 'cmd_vel', 10)
+        # サブスクライバー。センサの情報の受信器。データが届くと、callback関数が呼ばれる。
+        self.sub_amcl_pose = self.create_subscription(
+            PoseWithCovarianceStamped, 'amcl_pose', self.amcl_pose_callback, 10)
+
+        # Nav2のアクションクライアント生成
+        self.nav_client = ActionClient(
+            self, NavigateThroughPoses, 'navigate_through_poses')
+
+        self.pose = None
+        self.waypoints = self.create_waypoint_list()  # 巡回地点リスト
+        self.number_of_poses_remaining = -1  # 残りのウェイポイント数
+
+        self.timer = self.create_timer(
+            2.0, self.start_navigation)  # 2秒後にナビゲーション開始
+
+    def create_waypoint_list(self):
+        # 巡回したいウェイポイントの定義
+        poses = []
+        for p in [(2.67, 0.369), (7.32, 0.394), (7.05, 1.91), (4.3, 2.6)]:
+            poses.append(pose_stamped_from(p[0], p[1], 0.0))
+        return poses
+
+    def amcl_pose_callback(self, msg):
+        self.pose = pose2d_from_amcl(msg)
+        
+
+    def start_navigation(self):
+        if not self.nav_client.wait_for_server(timeout_sec=5.0):
+            self.get_logger().error("navigate_through_poses action server not available.")
+            raise KeyboardInterrupt()  # 終了
+            return
+
+        goal_msg = NavigateThroughPoses.Goal()
+        goal_msg.poses = self.waypoints
+        self.get_logger().info("Sending waypoints to Nav2.")
+        self.future = self.nav_client.send_goal_async(
+            goal_msg, feedback_callback=self.feedback_callback)
+        self.future.add_done_callback(self.goal_response_callback)
+        self.destroy_timer(self.timer)
+        self.timer = self.create_timer(
+            0.05, self.control_loop)  # コントロールループに切り替える
+
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().error('Goal rejected!')
+            raise KeyboardInterrupt()  # 終了
+            return
+
+        self.future = goal_handle.get_result_async()
+        self.future.add_done_callback(self.result_callback)
+
+    def feedback_callback(self, feedback_msg):
+        # 現在通過中のウェイポイントのインデックスを保存する
+        self.number_of_poses_remaining = feedback_msg.feedback.number_of_poses_remaining
+
+    def result_callback(self, result_msg):
+        # ナビゲーションの結果を受け取るコールバック
+        if result_msg.status == 4:  # SUCCEEDED
+            self.get_logger().info("Navigation succeeded!")
+        else:
+            self.get_logger().warn(
+                f"Navigation failed with status code: {result_msg.status}")
+        raise KeyboardInterrupt()  # 終了
+
+    def control_loop(self):
+        # タイマーで呼ばれる関数。ここにロボットの制御のコードを書く。
+        text = f"Poses remaining: {self.number_of_poses_remaining}"
+        if self.pose:
+            text += f", Current position: ({self.pose.x:.2f}, {self.pose.y:.2f}, {self.pose.theta:.2f})"
+        self.get_logger().info(text, throttle_duration_sec=1.0)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = None
+    try:
+        node = NavCommander()
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        if node:
+            node.destroy_node()
+        if rclpy.ok():
+            rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
